457e471671d78786126b52496d5a7d2d01808a88
Yvette Mamno Souop <yvettematache@gmail.com>
2026-09-16 21:49:53 -0400

    feat: create homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>NexaDesk</h1>
+      <p>Plateforme intelligente de gestion des demandes de support informatique.</p>
+    </div>
+  )
+}
+
+export default HomePage
